e45b8a5d77a497c67a88c0e6b092cc6f9ac81ef5
Kevin Reed <662069+kavun@users.noreply.github.com>
2023-12-02 23:12:36 -0600

    day 02 part 1

--- src/AOC23Console/Day02/Day02.cs
+++ src/AOC23Console/Day02/Day02.cs
@@ -1,17 +1,44 @@
-﻿namespace AOC23Console.Day02
+﻿using System.Text.RegularExpressions;
+
+namespace AOC23Console.Day02
 {
-    public class Day02 : IDay
+    public partial class Day02 : IDay
     {
         public string Name => "Day 2: Cube Conundrum";
 
+        public record Game(int Id, int Red, int Green, int Blue)
+        {
+            public bool IsPossible => Red <= 12 && Green <= 13 && Blue <= 14;
+        }
+
         public int Part1(string input)
         {
-            return 0;
+            return input.Split(Environment.NewLine)
+                .Select(line =>
+                {
+                    var id = int.Parse(GameIdRegex().Match(line).Groups[1].Value);
+                    var red = RedCubesRegex().Matches(line).Max(m => int.Parse(m.Groups[1].Value));
+                    var green = GreenCubesRegex().Matches(line).Max(m => int.Parse(m.Groups[1].Value));
+                    var blue = BlueCubesRegex().Matches(line).Max(m => int.Parse(m.Groups[1].Value));
+                    var g = new Game(Id: id, Red: red, Green: green, Blue: blue);
+                    return g;
+                })
+                .Where(g => g.IsPossible)
+                .Sum(g => g.Id);
         }
 
         public int Part2(string input)
         {
             return 0;
         }
+
+        [GeneratedRegex(@"^Game ([0-9]+):")]
+        private static partial Regex GameIdRegex();
+        [GeneratedRegex(@" ([0-9]+) red")]
+        private static partial Regex RedCubesRegex();
+        [GeneratedRegex(@" ([0-9]+) green")]
+        private static partial Regex GreenCubesRegex();
+        [GeneratedRegex(@" ([0-9]+) blue")]
+        private static partial Regex BlueCubesRegex();
     }
 }
--- src/AOC23Console/Program.cs
+++ src/AOC23Console/Program.cs
@@ -1,15 +1,36 @@
 ﻿using AOC23Console;
 using AOC23Console.Day01;
 using AOC23Console.Day02;
+using System.Diagnostics;
+
+Console.WriteLine();
+
+var sw = new Stopwatch();
 
 foreach (var day in new IDay[] {
     new Day01(),
     new Day02(),
 })
 {
+    var input = File.ReadAllText($"{day.GetType().Name}/input.txt");
+
+    var (part1, part1Time) = WithTimer(day.Part1, input);
+    var (part2, part2Time) = WithTimer(day.Part2, input);
+
     Console.WriteLine($"""
         {day.Name}
-          Part One: {day.Part1(File.ReadAllText($"{day.GetType().Name}/input.txt"))}
-          Part Two: {day.Part2(File.ReadAllText($"{day.GetType().Name}/input.txt"))}
+          Part One: {part1} ({part1Time}ms)
+          Part Two: {part2} ({part2Time}ms)
         """);
+
+    Console.WriteLine();
+}
+
+(int, long) WithTimer(Func<string, int> action, string input)
+{
+    var sw = new Stopwatch();
+    sw.Start();
+    var result = action(input);
+    sw.Stop();
+    return (result, sw.ElapsedMilliseconds);
 }
