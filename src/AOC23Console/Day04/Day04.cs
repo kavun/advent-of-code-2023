7755bc0cc792c27199f85eda2d372f3a9a8bcf1c
Kevin Reed <662069+kavun@users.noreply.github.com>
2023-12-11 18:42:58 +0000

    Day04 part 1

--- src/AOC23Console/Day04/Day04.cs
+++ src/AOC23Console/Day04/Day04.cs
@@ -8,7 +8,22 @@ public partial class Day04 : IDay
 
     public int Part1(string input)
     {
-        return 0;
+        return input.Split(Environment.NewLine)
+            .Sum(line => {
+                var parts = line.Split(":")[1].Split("|");
+                var winning = parts[0].Split(" ", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+                var have = parts[1].Split(" ", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+                var winningNums = new HashSet<int>(winning.Select(w => int.Parse(w)));
+                var haveNums = new HashSet<int>(have.Select(h => int.Parse(h)));
+
+                var winningCount = winningNums.Where(w => haveNums.Contains(w)).Count();
+                var score = (int)Math.Pow(2, winningCount - 1);
+            
+                // Console.WriteLine(line);
+                // Console.WriteLine($"2 ^ {winningCount} = {score}");
+
+                return score;
+            });
     }
 
     public int Part2(string input)
