c22d5b77dde6a8560daef2fe3205cb89bb2c7198
Kevin Reed <662069+kavun@users.noreply.github.com>
2023-12-11 19:17:39 +0000

    Day04 part 2

--- src/AOC23Console/Day04/Day04.cs
+++ src/AOC23Console/Day04/Day04.cs
@@ -1,5 +1,3 @@
-using System.Text;
-
 namespace AOC23Console.Day04;
 
 public partial class Day04 : IDay
@@ -9,25 +7,45 @@ public partial class Day04 : IDay
     public int Part1(string input)
     {
         return input.Split(Environment.NewLine)
-            .Sum(line => {
+            .Sum(line =>
+            {
                 var parts = line.Split(":")[1].Split("|");
                 var winning = parts[0].Split(" ", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
                 var have = parts[1].Split(" ", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
                 var winningNums = new HashSet<int>(winning.Select(w => int.Parse(w)));
                 var haveNums = new HashSet<int>(have.Select(h => int.Parse(h)));
-
                 var winningCount = winningNums.Where(w => haveNums.Contains(w)).Count();
                 var score = (int)Math.Pow(2, winningCount - 1);
-            
-                // Console.WriteLine(line);
-                // Console.WriteLine($"2 ^ {winningCount} = {score}");
-
                 return score;
             });
     }
 
     public int Part2(string input)
     {
-        return 0;
+        var lines = input.Split(Environment.NewLine);
+        var extras = new Dictionary<int, int>();
+        for (var lineNum = 0; lineNum < lines.Length; lineNum++) {
+            extras.Add(lineNum, 1);
+        }
+
+        var l = 0;
+        foreach (var line in lines)
+        {
+            var parts = line.Split(":")[1].Split("|");
+            var winning = parts[0].Split(" ", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+            var have = parts[1].Split(" ", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+            var winningNums = new HashSet<int>(winning.Select(w => int.Parse(w)));
+            var haveNums = new HashSet<int>(have.Select(h => int.Parse(h)));
+            var winningCount = winningNums.Where(w => haveNums.Contains(w)).Count();
+            var nextIdx = l + 1;
+            var max = Math.Min(lines.Length, nextIdx + winningCount);
+            for (var e = nextIdx; e < max; e++) {
+                extras[e] = extras[e] + extras[l];
+            }
+
+            l++;
+        }
+
+        return extras.Values.Sum();
     }
 }
