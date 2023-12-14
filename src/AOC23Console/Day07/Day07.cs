e6f85c2f16edc94e8c40f6c50302fdc09eb25a8e
Kevin Reed <662069+kavun@users.noreply.github.com>
2023-12-14 00:12:29 -0600

    Day07 Part 2

--- src/AOC23Console/Day07/Day07.cs
+++ src/AOC23Console/Day07/Day07.cs
@@ -21,6 +21,7 @@ public partial class Day07 : IDay
         {'K', 13},
         {'A', 14},
     };
+
     private static readonly Dictionary<int, char> _rankCards = _cardRanks.ToDictionary(kvp => kvp.Value, kvp => kvp.Key);
 
     public long Part1(string input)
@@ -91,8 +92,108 @@ public partial class Day07 : IDay
         public decimal Rank { get; }
     }
 
+    private static readonly Dictionary<char, int> _cardRanksPt2 = new()
+    {
+        {'2', 2},
+        {'3', 3},
+        {'4', 4},
+        {'5', 5},
+        {'6', 6},
+        {'7', 7},
+        {'8', 8},
+        {'9', 9},
+        {'T', 10},
+        {'J', 1},
+        {'Q', 12},
+        {'K', 13},
+        {'A', 14},
+    };
+
+    private static readonly Dictionary<int, char> _rankCardsPt2 = _cardRanksPt2.ToDictionary(kvp => kvp.Value, kvp => kvp.Key);
+
     public long Part2(string input)
     {
-        return 0;
+        var hands = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries)
+            .Select(line => line.Split(" ", StringSplitOptions.RemoveEmptyEntries))
+            .Select(line => new Hand2(line[0], line[1]));
+
+        var finalOrder = new List<Hand2>();
+        foreach (var rankGroup in hands.GroupBy(h => h.Rank).OrderByDescending(g => g.Key))
+        {
+            var rerankedGroup = rankGroup
+                .OrderByDescending(h => h.Cards[0])
+                .ThenByDescending(h => h.Cards[1])
+                .ThenByDescending(h => h.Cards[2])
+                .ThenByDescending(h => h.Cards[3])
+                .ThenByDescending(h => h.Cards[4]);
+            finalOrder.AddRange(rerankedGroup);
+        }
+
+        var total = 0;
+        var hIdx = finalOrder.Count;
+        foreach (var hand in finalOrder)
+        {
+            total += hand.Bid * hIdx;
+            hIdx--;
+        }
+
+        return total;
+    }
+
+    public class Hand2
+    {
+        public Hand2(string hand, string bid)
+        {
+            HandStr = hand;
+            Bid = int.Parse(bid);
+            Cards = HandStr.ToCharArray().Select(c => _cardRanksPt2[c]).ToArray();
+            var ogGrouped = Cards
+                .GroupBy(c => c)
+                .ToArray();
+            CardCounts = ogGrouped
+                .Where(g => g.Key != 1) // remove jokers
+                .OrderByDescending(g => g.Count())
+                .ToDictionary(g => g.Key, g => g.Count());
+            var counts = CardCounts.Values.ToArray();
+            var keys = CardCounts.Keys.ToArray();
+            var jokers = ogGrouped.Where(g => g.Key == 1).Sum(g => g.Count());
+
+            var potentialCounts = new List<Dictionary<int, int>>();
+            for (var i = 0; i < counts.Length; i++)
+            {
+                var potentialCount = new Dictionary<int, int>(CardCounts);
+                potentialCount[keys[i]] += jokers;
+                potentialCount = potentialCount
+                    .Where(kvp => kvp.Key != 1) // remove jokers
+                    .OrderByDescending(kvp => kvp.Value)
+                    .ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+                potentialCounts.Add(potentialCount);
+            }
+
+            Rank = potentialCounts.Count == 0 ? 5 : potentialCounts.Select(c => c.Count == 0 ? 5 : c.First().Value switch
+            {
+                5 => 5,
+                4 => 4,
+                3 => 3 + (counts[1] == 2 ? .1m : 0),
+                2 => 2 + (counts[1] == 2 ? .1m : 0),
+                _ => -1
+            }).Max();
+        }
+
+        public int[] Cards { get; }
+        public Dictionary<int, int> CardCounts { get; }
+
+        public string HandStr { get; }
+        public int Bid { get; }
+        public override string ToString()
+        {
+            return $"""
+                {HandStr} {Bid}
+                {string.Join(" ", Cards)}
+                  {string.Join("\n  ", CardCounts.Select(kvp => $"{_rankCardsPt2[kvp.Key]}:{kvp.Value}"))}
+                """;
+        }
+
+        public decimal Rank { get; }
     }
 }
--- tests/AOC23Tests/Day07Tests.cs
+++ tests/AOC23Tests/Day07Tests.cs
@@ -19,8 +19,12 @@ public class Day07Tests
     [Fact]
     public void Part2()
     {
-        Assert.Equal(0, new Day07().Part2("""
-
+        Assert.Equal(5905, new Day07().Part2("""
+            32T3K 765
+            T55J5 684
+            KK677 28
+            KTJJT 220
+            QQQJA 483
             """));
     }
 }
