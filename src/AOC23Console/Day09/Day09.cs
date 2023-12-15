7a39f2cf38c071df8027a11c7a0d347c108e9dda
Kevin Reed <662069+kavun@users.noreply.github.com>
2023-12-15 16:20:10 -0500

    Day09 Part 0

---
+++ src/AOC23Console/Day09/Day09.cs
@@ -0,0 +1,17 @@
+namespace AOC23Console.Day09;
+
+public partial class Day09 : IDay
+{
+    public string Number => "09";
+    public string Name => $"Day {Number}: Mirage Maintenance";
+
+    public long Part1(string input)
+    {
+        return 0;
+    }
+
+    public long Part2(string input)
+    {
+        return 0;
+    }
+}
---
+++ tests/AOC23Tests/Day09Tests.cs
@@ -0,0 +1,24 @@
+﻿using AOC23Console.Day09;
+
+namespace AOC23Tests;
+
+public class Day09Tests
+{
+    [Fact]
+    public void Part1()
+    {
+        Assert.Equal(114, new Day09().Part1("""
+            0 3 6 9 12 15
+            1 3 6 10 15 21
+            10 13 16 21 30 45
+            """));
+    }
+
+    [Fact]
+    public void Part2()
+    {
+        Assert.Equal(0, new Day09().Part2("""
+
+            """));
+    }
+}
