b1eb8ff4fd7b5e43ecccddb078aecb79aa90d122
Kevin Reed <662069+kavun@users.noreply.github.com>
2023-12-15 14:36:48 -0500

    Day08 Part 0

---
+++ src/AOC23Console/Day08/Day08.cs
@@ -0,0 +1,17 @@
+namespace AOC23Console.Day08;
+
+public partial class Day08 : IDay
+{
+    public string Number => "08";
+    public string Name => $"Day {Number}: Haunted Wasteland";
+
+    public long Part1(string input)
+    {
+        return 0;
+    }
+
+    public long Part2(string input)
+    {
+        return 0;
+    }
+}
---
+++ tests/AOC23Tests/Day09Tests.cs
@@ -0,0 +1,38 @@
+﻿using AOC23Console.Day08;
+
+namespace AOC23Tests;
+
+public class Day08Tests
+{
+    [Fact]
+    public void Part1()
+    {
+        Assert.Equal(2, new Day08().Part1("""
+            RL
+
+            AAA = (BBB, CCC)
+            BBB = (DDD, EEE)
+            CCC = (ZZZ, GGG)
+            DDD = (DDD, DDD)
+            EEE = (EEE, EEE)
+            GGG = (GGG, GGG)
+            ZZZ = (ZZZ, ZZZ)
+            """));
+
+        Assert.Equal(6, new Day08().Part1("""
+            LLR
+
+            AAA = (BBB, BBB)
+            BBB = (AAA, ZZZ)
+            ZZZ = (ZZZ, ZZZ)
+            """));
+    }
+
+    [Fact]
+    public void Part2()
+    {
+        Assert.Equal(0, new Day08().Part2("""
+
+            """));
+    }
+}
