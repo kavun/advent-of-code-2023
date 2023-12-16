6aef638e763ef05638b357a9e6e91c73e7b51477
Kevin Reed <662069+kavun@users.noreply.github.com>
2023-12-16 11:53:03 -0500

    Day09 Part 02

--- src/AOC23Console/Day09/Day09.cs
+++ src/AOC23Console/Day09/Day09.cs
@@ -54,6 +54,48 @@ public partial class Day09 : IDay
 
     public long Part2(string input)
     {
-        return 0;
+        var lines = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+        return lines.Sum(line =>
+        {
+            var nums = line.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToList();
+            var subs = new List<List<long>>() { nums };
+            var allZero = false;
+            var nextSub = nums;
+            while (!allZero)
+            {
+                allZero = true;
+                var sub = new List<long>();
+                for (var i = 0; i < nextSub.Count - 1; i++)
+                {
+                    var a = nextSub[i];
+                    var b = nextSub[i + 1];
+                    var result = b - a;
+                    if (result != 0)
+                    {
+                        allZero = false;
+                    }
+                    sub.Add(result);
+                }
+
+                subs.Add(sub);
+                nextSub = sub;
+            }
+
+            long GetNextValue(int currIdx)
+            {
+                var next = 0L;
+                if (currIdx >= 0 && currIdx < subs.Count - 1)
+                {
+                    next = GetNextValue(currIdx + 1);
+                }
+                var first = subs[currIdx].First();
+                // Console.WriteLine($"{string.Join(" ", subs[currIdx])} + {last + next}");
+                return first - next;
+            }
+
+            var next = GetNextValue(0);
+            // Console.WriteLine();
+            return next;
+        });
     }
 }
--- tests/AOC23Tests/Day09Tests.cs
+++ tests/AOC23Tests/Day09Tests.cs
@@ -17,8 +17,10 @@ public class Day09Tests
     [Fact]
     public void Part2()
     {
-        Assert.Equal(0, new Day09().Part2("""
-
+        Assert.Equal(2, new Day09().Part2("""
+            0 3 6 9 12 15
+            1 3 6 10 15 21
+            10 13 16 21 30 45
             """));
     }
 }
