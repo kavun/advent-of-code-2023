9dff435999a7223cd49bd6f762e3973925107607
Kevin Reed <662069+kavun@users.noreply.github.com>
2023-12-16 13:51:05 -0500

    Day10 Part 0

---
+++ src/AOC23Console/Day10/Day10.cs
@@ -0,0 +1,17 @@
+namespace AOC23Console.Day10;
+
+public partial class Day10 : IDay
+{
+    public string Number => "10";
+    public string Name => $"Day {Number}: Pipe Maze";
+
+    public long Part1(string input)
+    {
+        return 0;
+    }
+
+    public long Part2(string input)
+    {
+        return 0;
+    }
+}
---
+++ tests/AOC23Tests/Day10Tests.cs
@@ -0,0 +1,26 @@
+﻿using AOC23Console.Day10;
+
+namespace AOC23Tests;
+
+public class Day10Tests
+{
+    [Fact]
+    public void Part1()
+    {
+        Assert.Equal(4, new Day10().Part1("""
+            .....
+            .S-7.
+            .|.|.
+            .L-J.
+            .....
+            """));
+    }
+
+    [Fact]
+    public void Part2()
+    {
+        Assert.Equal(0, new Day10().Part2("""
+
+            """));
+    }
+}
