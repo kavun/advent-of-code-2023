4cfe33d955f418f0161957614e2271b4278073ea
Kevin Reed <662069+kavun@users.noreply.github.com>
2023-12-16 11:49:46 -0500

    Day09 Part 1

--- src/AOC23Console/Day09/Day09.cs
+++ src/AOC23Console/Day09/Day09.cs
@@ -7,7 +7,49 @@ public partial class Day09 : IDay
 
     public long Part1(string input)
     {
-        return 0;
+        var lines = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+        return lines.Sum(line =>
+        {
+            var nums = line.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToList();
+            var subs = new List<List<long>>() { nums };
+            var allZero = false;
+            var nextSub = nums;
+            while (!allZero)
+            {
+                allZero = true;
+                var sub = new List<long>();
+                for (var i = 0; i < nextSub.Count - 1; i++)
+                {
+                    var a = nextSub[i];
+                    var b = nextSub[i + 1];
+                    var result = b - a;
+                    if (result != 0)
+                    {
+                        allZero = false;
+                    }
+                    sub.Add(result);
+                }
+
+                subs.Add(sub);
+                nextSub = sub;
+            }
+
+            long GetNextValue(int currIdx)
+            {
+                var next = 0L;
+                if (currIdx >= 0 && currIdx < subs.Count - 1)
+                {
+                    next = GetNextValue(currIdx + 1);
+                }
+                var last = subs[currIdx].Last();
+                // Console.WriteLine($"{string.Join(" ", subs[currIdx])} + {last + next}");
+                return last + next;
+            }
+
+            var next = GetNextValue(0);
+            // Console.WriteLine();
+            return next;
+        });
     }
 
     public long Part2(string input)
